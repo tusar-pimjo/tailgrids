358d3275906e04b6eafc38d2a09b884e0e61d4d0
Johurul Haque <johurul@pimjo.com>
2025-11-22 08:04:34 +0600

    fix: raw base url for components

--- packages/cli/src/constants/urls.ts
+++ packages/cli/src/constants/urls.ts
@@ -1,2 +1,2 @@
 export const COMPONENT_REGISTRY_RAW_BASE_URL =
-  'https://raw.githubusercontent.com/TailGrids/oss/main/refs/heads/main/apps/docs/src/registry';
+  'https://raw.githubusercontent.com/TailGrids/oss/refs/heads/main/apps/docs/src/registry';
