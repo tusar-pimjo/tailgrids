420ba1a370f4b2d14afdab852a74b12507f6405a
Johurul Haque <johurul@pimjo.com>
2025-11-24 09:57:02 +0600

    refactor: Remove unused documentation files and update main documentation structure
    - Deleted `usage.mdx` and `test.mdx` files
    - Updated `index.mdx` to revising with the introduction and features of TailGrids.
    - Improved the layout and content of the `HeaderBanner` component with missing dark classes.
    - Refactored `ContainerGrid` component for cleaner code and improved readability.
    - Adjusted global CSS for consistency and better theme support.
    - Modified layout shared properties to include new links and updated navigation structure.

--- apps/docs/src/components/header-banner.tsx
+++ apps/docs/src/components/header-banner.tsx
@@ -1,32 +1,32 @@
 import Image from "next/image";
 
 export default function HeaderBanner() {
-  return (
-    <div className="bg-gray-50 rounded-3xl relative overflow-hidden">
-      <Image
-        src="/images/glow.png"
-        alt="Glow"
-        width={100}
-        height={0}
-        className="w-full h-full rounded-r-3xl absolute top-0 right-0 m-0!"
-      />
-      <div className="absolute top-0 right-0">
-        <Image
-          src="/images/logo-banner.png"
-          alt="Logo Banner"
-          width="0"
-          height="0"
-          sizes="100vw"
-          className="w-full h-auto m-0!"
-        />
-      </div>
-      <div className="px-8 py-7  relative z-20">
-        <h3 className="m-0!">Quick Start</h3>
-        <p>
-          Browse and effortlessly copy-paste from over 800+ components and
-          templates.
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-gray-50 dark:bg-gray-900 rounded-3xl relative overflow-hidden mb-8">
+			<Image
+				src="/images/glow.png"
+				alt="Glow"
+				width={100}
+				height={0}
+				className="w-full h-full rounded-r-3xl absolute top-0 right-0 m-0!"
+			/>
+			<div className="absolute top-0 right-0">
+				<Image
+					src="/images/logo-banner.png"
+					alt="Logo Banner"
+					width="0"
+					height="0"
+					sizes="100vw"
+					className="w-full h-auto m-0!"
+				/>
+			</div>
+			<div className="px-8 py-7  relative z-20">
+				<h3 className="mt-0 mb-3 text-2xl">Quick Start</h3>
+				<p className="mb-0 text-[#6B7280] dark:text-[#9CA3AF] text-sm font-normal max-w-100">
+					Browse and effortlessly copy-paste from over 800+ components and
+					templates.
+				</p>
+			</div>
+		</div>
+	);
 }
