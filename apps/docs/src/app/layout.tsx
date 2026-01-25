c3d3f5704d37a440c29e3ea8104328dd0b65d79e
Johurul Haque <johurul@pimjo.com>
2026-01-25 18:25:01 +0600

    refactor: Move Google Sign-In client script into a head tag.

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -44,7 +44,10 @@ export default function Layout({ children }: LayoutProps<"/">) {
       className={`${dmSans.variable} ${geistMono.variable}`}
       suppressHydrationWarning
     >
-      <script src="https://accounts.google.com/gsi/client" async defer />
+      <head>
+        <script src="https://accounts.google.com/gsi/client" async defer />
+      </head>
+
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
 
       <body className="flex flex-col min-h-screen bg-white dark:bg-[#030712] antialiased">
